5c0b351914d68ffe3ebd3d70325374a369a31f06
erru227 <erru227@uky.edu>
2026-09-23 13:54:44 -0400

    Update Script.js
    Changing, hopefully for the last time

--- Script.js
+++ Script.js
@@ -1,14 +1,16 @@
-let header = document.getElementById("header");
+let new_rule_index = -1;
 let css = document.styleSheets[0];
 
 function add_rule(){
     css.insertRule("h1{ text-shadow: 2px 2px 5px #2F23FA;}");
-}
-function change_rule(){
-    header.style.setProperty("text-shadow", "2px 2px 5px rgb(250, 35, 92)");
-}
-function delete_rule(){
     for (let i = 0; i < css.cssRules.length; i++)
         if (css.cssRules[i].selectorText === "h1")
-            css.deleteRule(i);
+            new_rule_index=i;
+}
+function change_rule(){
+    css.cssRules[new_rule_index].style.setProperty("text-shadow-color", "rgb(250, 35, 92)");
+}
+function delete_rule(){
+    css.deleteRule(new_rule_index);
+    new_rule_index=-1;
 }
