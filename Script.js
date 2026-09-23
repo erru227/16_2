a4c46df4221fa3bbb8481d629542eba5f81f9072
erru227 <erru227@uky.edu>
2026-09-23 13:33:26 -0400

    Update Script.js
    Testing

--- Script.js
+++ Script.js
@@ -5,6 +5,7 @@ function add_rule(){
     css.insertRule("h1{ text-shadow: 2px 2px 5px #2F23FA;}");
 }
 function change_rule(){
+    header.textContent="Working 2";
     header.style.setProperty("text-shadow-color", "rgb(250, 35, 92)");
 }
 function delete_rule(){
