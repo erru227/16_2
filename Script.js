b52486c8c10af2c1add647372842938e1f1e46ec
erru227 <erru227@uky.edu>
2026-09-23 13:44:53 -0400

    Update Script.js
    Testing

--- Script.js
+++ Script.js
@@ -3,16 +3,13 @@ let css = document.styleSheets[0];
 
 function add_rule(){
     css.insertRule("h1{ text-shadow: 2px 2px 5px #2F23FA;}");
-    header.textContent="Working 3";
 }
 function change_rule(){
-    header.textContent="Working 2";
-    header.style.setProperty("text-shadow-color", "rgb(250, 35, 92)");
+    header.style.setProperty("text-shadow", "2px 2px 5px rgb(250, 35, 92)");
 }
 function delete_rule(){
     let new_rule_index = css.cssRules.length-1;
-    if (css.cssRules[new_rule_index].selectorText === "h1"){
+    header.textContent=new_rule_index+" "+css.cssRules[new_rule_index].selectorText;
+    if (css.cssRules[new_rule_index].selectorText === "h1")
         css.deleteRule(new_rule_index);
-        header.textContent="Working";
-    }
 }
