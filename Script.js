74be07ace77ca768e5905539a3a72549c0639869
erru227 <erru227@uky.edu>
2026-09-23 13:48:02 -0400

    Update Script.js
    Trying, have first two down, just need delete to work

--- Script.js
+++ Script.js
@@ -8,8 +8,7 @@ function change_rule(){
     header.style.setProperty("text-shadow", "2px 2px 5px rgb(250, 35, 92)");
 }
 function delete_rule(){
-    let new_rule_index = css.cssRules.length-1;
-    header.textContent=new_rule_index+" "+css.cssRules[new_rule_index].selectorText;
-    if (css.cssRules[new_rule_index].selectorText === "h1")
-        css.deleteRule(new_rule_index);
+    for (let i = 0; i < css.cssRules.length; i++)
+        if (css.cssRules[i].selectorText === "h1")
+            css.deleteRule(i);
 }
